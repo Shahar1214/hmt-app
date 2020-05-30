308edf3eba4ce19a008db7d2bf46c941efcb7393
Shahar1214 <65449285+Shahar1214@users.noreply.github.com>
2020-05-30 16:56:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Birkman Ya Tahat, How many times? {} times!\n'.format(count)
+    return 'Goldman Ya Tahat, How many times? {} times!\n'.format(count)
